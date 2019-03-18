a4847172d9a3b737a62f413d7b5ddb06dd2ce0a0
ssganesh1407@gmail.com <getmypassword03>
2019-03-18 21:31:03 +0530

    Signed-off-by: selvah

---
+++ Device/DeviceProfile.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="DeviceProfile.asmx.cs" Class="Device.DeviceProfile" %>
---
+++ Device/DeviceProfile.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Device
+{
+    /// <summary>
+    /// Summary description for DeviceProfile
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class DeviceProfile : System.Web.Services.WebService
+    {
+       
+        [WebMethod]
+        public string DeviceConnector(string action)
+        {
+            Device.DeviceConnector.jinp = action;
+            Device.DeviceConnector dc = new Device.DeviceConnector();
+            dc.openPort();
+            string resp = dc.sendData();
+            dc.closePort();
+            return resp;
+        }
+        
+    }
+}
